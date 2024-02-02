7b0c5e291daeff6e96927a25cbfe9269b9ee27cf
Mikkel Paltorp Schmitt <mpasc@elektro.dtu.dk>
2024-02-02 21:08:57 +0100

    Update test_blockdiagonal.jl
     - Stop using `Symmtric` in test. Instead use dense matrices.
     - Adding tests for matrix functions.

--- test/test_blockdiagonal.jl
+++ test/test_blockdiagonal.jl
@@ -2,13 +2,13 @@ using BlockDiagonalMatrices
 using LinearAlgebra
 using SparseArrays
 using StaticArrays
+using Test
 
 function get_random_sized_psdm(n)
     b1 = rand(n,n);
     Q = qr(b1).Q
     B1 = Q*Diagonal((rand(n) .+ 0.5 ))*Q'
-    B1 = Symmetric(B1)
-    return B1
+    return (B1 + B1')/2
 end
 
 #### Testing equal block sizes
@@ -38,6 +38,18 @@ SB = sparse(B)
 @test B[2n,n] == SB[2n,n]
 @test B[1,1] == SB[1,1]
 @test B[end,end] == SB[end,end]
+
+B_dense = Matrix(B)
+b1 = size(B.blocks[1],1)
+bn = size(B.blocks[end],1) - 1
+for func in (:log, :sqrt, :sin, :tan, :cos, :sinh, :tanh)
+    @eval begin
+        func_dense = ($func)(B_dense)
+        func_block = ($func)(B)
+        @test func_dense[1:b1,1:b1] ≈ func_block.blocks[1]
+        @test func_dense[end-bn:end,end-bn:end] ≈ func_block.blocks[end]
+    end
+end
 
 
 # Make 3x3 blocks - But with StaticArrays for speedup
@@ -75,6 +87,17 @@ Xr = randn(size(Br,1),3)
 @test A*Xr ≈ Br*Xr
 @test A\Xr ≈ Br\Xr
 
+A_dense = Matrix(Br)
+r1 = size(Br.blocks[1],1)
+rn = size(Br.blocks[end],1) - 1
+for func in (:log, :sqrt, :sin, :tan, :cos, :sinh, :tanh)
+    @eval begin
+        func_dense = ($func)(A_dense)
+        func_block = ($func)(Br)
+        @test func_dense[1:r1,1:r1] ≈ func_block.blocks[1]
+        @test func_dense[end-rn:end,end-rn:end] ≈ func_block.blocks[end]
+    end
+end
 
 # Testing logs, determinants, and variants
 @test logdet(A) ≈ logdet(Br)
