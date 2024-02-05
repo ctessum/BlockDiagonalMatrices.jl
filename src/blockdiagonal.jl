d28f553b0c1c58b6578c47af765d6ab9bb84fbff
Mikkel Paltorp Schmitt <mpasc@elektro.dtu.dk>
2024-02-05 09:39:50 +0100

    Inverse by direct inversion of blocks
    Probably not the best solution, but so far it is the easiest approach

--- src/blockdiagonal.jl
+++ src/blockdiagonal.jl
@@ -62,12 +62,11 @@ function Base.getindex(B::BlockDiagonal{T},i::Integer,j::Integer) where {T}
     else
         block_no = ls.stop
     end
-    # Find-block - Return 0 if now in column block
-    # if j ∈ (col_indicies[ls.stop] + 1):col_indicies[ls.start]
+    # Find-block - Return 0 if not in column block
     if j ∈ (col_indicies[block_no] + 1):col_indicies[block_no+1]
         return blocks(B)[block_no][i - row_indicies[block_no], j - col_indicies[block_no]]
     else
-        return zero(eltype(B))
+        return zero(T)
     end
 end
 
@@ -110,6 +109,21 @@ function SparseArrays.sparse(B::BlockDiagonal{T}) where {T}
     return SparseArrays.sparse(I,J,V)
 end
 
+# Does not seem to be faster than [inv(block) for block in blocks(B)]
+function _create_inverse_blocks(blocks)
+    W = similar(blocks)
+    @floop @inbounds for (block_id,block) in enumerate(blocks)
+        W[block_id] = inv(block)
+    end
+    return W
+end
+function Base.inv(B::BlockDiagonal)
+    return BlockDiagonal([inv(block) for block in blocks(B)], _extract_block_information(B)...)
+end
+function inv_sparse(B::BlockDiagonal{T}) where {T}
+    return sparse(inv(B))
+end
+
 function LinearAlgebra.mul!(y::AbstractVecOrMat{T}, B::BlockDiagonal{T,V}, x::AbstractVecOrMat{T}) where {T,V}
     @floop @inbounds for (block_id, block) in enumerate(blocks(B))
         block_row_start = B.block_row_indices[block_id] + 1
@@ -122,10 +136,35 @@ function LinearAlgebra.mul!(y::AbstractVecOrMat{T}, B::BlockDiagonal{T,V}, x::Ab
 end
 
 
+
+
+
+function LinearAlgebra.:\(B::BlockDiagonal, x::AbstractVecOrMat)
+    if !B.is_block_square
+        throw(DimensionMismatch("Not all blocks are square. Use a sparse format instead i.e. sparse(B)."))
+    end
+    y = similar(x,promote_type(eltype(B),eltype(x)),size(x))
+    @floop @inbounds for (block_id,block) in enumerate(blocks(B))
+        block_start = B.block_row_indices[block_id] + 1
+        block_end   = B.block_row_indices[block_id + 1]
+        # Some problems with ldiv! when using sparse blocks
+        # F = cholesky(block)
+        # F = factorize(block)
+        # @views ldiv!(y[block_start:block_end,:], F, x[block_start:block_end,:])
+        @views y[block_start:block_end,:] = block \ x[block_start:block_end,:]
+    end
+    return y
+end
+
 LinearAlgebra.logdet(B::BlockDiagonal) = B.is_block_square ? sum(logdet, blocks(B)) : error("Not all blocks are square")
 LinearAlgebra.det(B::BlockDiagonal) = B.is_block_square ? prod(det, blocks(B)) : error("Not all blocks are square")
-LinearAlgebra.tr(B::BlockDiagonal) = B.is_block_square ? sum(tr, blocks(B)) : sum(diag(B))
-
+function LinearAlgebra.tr(B::BlockDiagonal)
+    if _is_square(B)
+        B.is_block_square ? sum(tr, blocks(B)) : sum(diag(B))
+    else
+        throw(DimensionMismatch("matrix is not square: dimensions are $(size(B))"))
+    end
+end
 
 Base.transpose(B::BlockDiagonal) = BlockDiagonal([transpose(block) for block in blocks(B)], B.block_col_indices, B.block_row_indices, B.is_block_square)
 Base.adjoint(B::BlockDiagonal) = BlockDiagonal([transpose(conj(block)) for block in blocks(B)], B.block_col_indices, B.block_row_indices, B.is_block_square)
@@ -164,36 +203,19 @@ function Base.:*(B::BlockDiagonal,C::BlockDiagonal)
 end
 
 
-function LinearAlgebra.:\(B::BlockDiagonal, x::AbstractVecOrMat)
+function LinearAlgebra.:\(B::BlockDiagonal, C::BlockDiagonal)
     if !B.is_block_square
         throw(DimensionMismatch("Not all blocks are square. Use a sparse format instead i.e. sparse(B)."))
     end
-    y = zeros(promote_type(eltype(B),eltype(x)),size(x))
-    @floop @inbounds for (block_id,block) in enumerate(blocks(B))
-        block_start = B.block_row_indices[block_id] + 1
-        block_end   = B.block_row_indices[block_id + 1]
-        # F = cholesky(block)
-        # F = factorize(block)
-        # Some problems with ldiv! when using sparse blocks
-        # @views ldiv!(y[block_start:block_end,:], F, x[block_start:block_end,:])
-        @views y[block_start:block_end,:] = block \ x[block_start:block_end,:]
-    end
-    return y
-end
-
-function LinearAlgebra.:\(B::BlockDiagonal, C::BlockDiagonal)
     if !(B.block_col_indices == C.block_row_indices)
         throw(DimensionMismatch("The block sizes of A and B are not compatible"))
-    end
-    if !B.is_block_square
-        throw(DimensionMismatch("Not all blocks are square. Use a sparse format instead i.e. sparse(B)."))
     end
     W = [zeros(promote_type(eltype(B),eltype(C)),size(B_block,1), size(C_block,2)) for (B_block, C_block) in zip(blocks(B),blocks(C))]
     @floop @inbounds for (block_id, (B_block, C_block)) in enumerate(zip(blocks(B),blocks(C)))
         L = factorize(B_block)
         # ldiv! have problems with some sparse arrays
-        @views W[block_id] = L \ C_block
         # @views ldiv!(W[block_id], L , C_block)
+        W[block_id] .= L \ C_block
     end
     is_block_square = all(block -> _is_square(block), W)
     return BlockDiagonal(W,B.block_row_indices, C.block_col_indices, is_block_square)
@@ -210,4 +232,52 @@ end
 #             return BlockDiagonal([($func)(block) for block in blocks(B)], _extract_block_information(B)...)
 #         end
 #     end
+# end
+
+
+function LinearAlgebra.:*(B::BlockDiagonal, x::AbstractSparseArray)
+    return sparse(B)*x
+end
+function LinearAlgebra.:\(B::BlockDiagonal, x::AbstractSparseArray)
+    # NB this based on direct inversion of blocks - Mqaybe not stable?
+    return inv_sparse(B)*x
+end
+
+
+### First attempts at making solove with sparse rhs faster
+# they're faster - But stil slower than casting the block-diagonal to sparse first;
+# function LinearAlgebra.:*(B::BlockDiagonal, x::AbstractSparseArray)
+#     Ii = zeros(Int64, 0)
+#     Jj = zeros(Int64, 0)
+#     Vv = zeros(promote_type(eltype(B),eltype(x)),0)
+#     # @floop @inbounds for (block_id, block) in enumerate(blocks(B))
+#     @inbounds for (block_id, block) in enumerate(B.blocks)
+#         block_idx = B.block_row_indices[block_id] + 1:B.block_row_indices[block_id + 1]
+#         xview = @view x[block_idx,:]
+#         idx = unique(sort!(vcat([xview[i,:].nzind for i in 1:size(xview,1)]...)))
+#         v = block*xview[:,idx]
+#         # Probably not good when running @floops
+#         append!(Ii,repeat(block_idx,length(idx)))
+#         append!(Jj,repeat(idx,inner=length(block_idx)))
+#         append!(Vv,v[:])
+#     end
+#     return sparse(Ii,Jj,Vv,size(B,1),size(x,1))
+# end
+# function LinearAlgebra.:\(B::BlockDiagonal, x::AbstractSparseArray)
+#     Ii = zeros(Int64, 0)
+#     Jj = zeros(Int64, 0)
+#     Vv = zeros(promote_type(eltype(B),eltype(x)),0)
+#     # Floop not possible due to append!?
+#     # @floop @inbounds for (block_id, block) in enumerate(blocks(B))
+#     @inbounds for (block_id, block) in enumerate(B.blocks)
+#         block_idx = B.block_row_indices[block_id] + 1:B.block_row_indices[block_id + 1]
+#         xview = @view x[block_idx,:]
+#         idx = unique(sort!(vcat([xview[i,:].nzind for i in 1:size(xview,1)]...)))
+#         v = block\Matrix(xview[:,idx])
+#         # Probably not good when running @floops
+#         append!(Ii,repeat(block_idx,length(idx)))
+#         append!(Jj,repeat(idx,inner=length(block_idx)))
+#         append!(Vv,v[:])
+#     end
+#     return sparse(Ii,Jj,Vv,size(B,1),size(x,1))
 # end
